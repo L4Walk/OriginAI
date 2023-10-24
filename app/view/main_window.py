3c76c6623f17791b54681f16d008316decef527a
L4Walk <1005058349@qq.com>
2023-10-24 16:42:32 +0800

    提交更改

---
+++ app/view/main_window.py
@@ -0,0 +1,104 @@
+# coding: utf-8
+from PyQt6.QtCore import Qt, pyqtSignal, QEasingCurve, QUrl, QSize
+from PyQt6.QtGui import QIcon, QDesktopServices
+from PyQt6.QtWidgets import QApplication, QHBoxLayout, QFrame, QWidget
+
+from qfluentwidgets import (NavigationAvatarWidget, NavigationItemPosition, MessageBox, FluentWindow,
+                            SplashScreen)
+from qfluentwidgets import FluentIcon as FIF
+
+from .gallery_interface import GalleryInterface
+from .home_interface import HomeInterface
+from .status_info_interface import StatusInfoInterface
+from .setting_interface import SettingInterface
+from ..common.config import SUPPORT_URL, cfg
+from ..common.icon import Icon
+from ..common.signal_bus import signalBus
+from ..common.translator import Translator
+from ..common import resource
+
+
+class MainWindow(FluentWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.initWindow()
+
+        # create sub interface
+        self.homeInterface = HomeInterface(self)
+        self.statusInfoInterface = StatusInfoInterface(self)
+        self.settingInterface = SettingInterface(self)
+        # enable acrylic effect
+        self.navigationInterface.setAcrylicEnabled(True)
+
+        self.connectSignalToSlot()
+
+        # add items to navigation interface
+        self.initNavigation()
+        self.splashScreen.finish()
+
+    def connectSignalToSlot(self):
+        signalBus.micaEnableChanged.connect(self.setMicaEffectEnabled)
+        signalBus.switchToSampleCard.connect(self.switchToSample)
+        signalBus.supportSignal.connect(self.onSupport)
+
+    def initNavigation(self):
+        # add navigation items
+        t = Translator()
+        self.addSubInterface(self.homeInterface, FIF.HOME, self.tr('Home'))
+
+        self.navigationInterface.addSeparator()
+
+        pos = NavigationItemPosition.SCROLL
+
+        # add custom widget to bottom
+        self.navigationInterface.addWidget(
+            routeKey='avatar',
+            widget=NavigationAvatarWidget('L4Walk', ':/gallery/images/l4walk.png'),
+            onClick=self.onSupport,
+            position=NavigationItemPosition.BOTTOM
+        )
+        self.addSubInterface(
+            self.settingInterface, FIF.SETTING, self.tr('Settings'), NavigationItemPosition.BOTTOM)
+
+    def initWindow(self):
+        self.resize(960, 780)
+        self.setMinimumWidth(760)
+        self.setWindowIcon(QIcon(':/gallery/images/logo.png'))
+        self.setWindowTitle('Origin AI')
+
+        self.setMicaEffectEnabled(cfg.get(cfg.micaEnabled))
+
+        # create splash screen
+        self.splashScreen = SplashScreen(self.windowIcon(), self)
+        self.splashScreen.setIconSize(QSize(106, 106))
+        self.splashScreen.raise_()
+
+        desktop = QApplication.screens()[0].availableGeometry()
+        w, h = desktop.width(), desktop.height()
+        self.move(w//2 - self.width()//2, h//2 - self.height()//2)
+        self.show()
+        QApplication.processEvents()
+
+    def onSupport(self):
+        w = MessageBox(
+            'Origin AI',
+            '致力于打造方便的AI Agents，如果你觉得这个项目有趣，可以考虑请作者喝一瓶快乐水🥤。您的支持就是作者开发和维护项目的动力🚀',
+            self
+        )
+        w.yesButton.setText('来啦老弟')
+        w.cancelButton.setText('下次一定')
+        if w.exec():
+            QDesktopServices.openUrl(QUrl(SUPPORT_URL))
+
+    def resizeEvent(self, e):
+        super().resizeEvent(e)
+        self.splashScreen.resize(self.size())
+
+    def switchToSample(self, routeKey, index):
+        """ switch to sample """
+        interfaces = self.findChildren(GalleryInterface)
+        for w in interfaces:
+            if w.objectName() == routeKey:
+                self.stackedWidget.setCurrentWidget(w, False)
+                w.scrollToCard(index)
